8fc4c7e2722d76eac5a4b80a1738e259063cd8b9
Setsu Fujita <fjt.seth@gmail.com>
2025-04-21 02:46:07 +0900

    feat: add tty option and history search to zsh script

--- scripts/zsh-duckhist.zsh
+++ scripts/zsh-duckhist.zsh
@@ -1,5 +1,15 @@
 # duckhist zsh integration
 duckhist_add_history() {
-    duckhist add -- "$1"
+    duckhist add --tty "$TTY" -- "$1"
 }
 zshaddhistory_functions+=("duckhist_add_history")
+
+
+function duckhist-history-selection() {
+    BUFFER=`duckhist search`
+    CURSOR=$#BUFFER
+    zle reset-prompt
+}
+
+zle -N duckhist-history-selection
+bindkey '^R' duckhist-history-selection
